3021add3928c43ad19e1d65d76125ef08ba06e95
Vorteo <92757545+Vorteo@users.noreply.github.com>
2022-11-15 19:47:32 +0100

    Add files via upload

---
+++ 15_11/Models.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef MODELS_H
+#define MODELS_H
+#include "Models/sphere.h"
+#include "Models/suzi_flat.h"
+#include "Models/suzi_smooth.h"
+#include "Models/plain.h"
+#include "Models/bushes.h"
+#include "Models/gift.h"
+#include "Models/tree.h"
+#endif
---
+++ 15_11/main.cpp
@@ -0,0 +1,35 @@
+
+#include "Application.h";
+
+int main(void)
+{
+	Application* application = application->GetInstance();
+
+	int sceneType;
+
+	printf("Scene type 1-5: ");
+	scanf_s("%d", &sceneType);
+
+	switch (sceneType)
+	{
+	case 1:
+		application->setScene(Factory::GetInstance()->createSpheresScene());
+		break;
+	case 2:
+		application->setScene(Factory::GetInstance()->createOneSphereScene());
+		break;
+	case 3:
+		application->setScene(Factory::GetInstance()->createMultipleObjectsScene());
+		break;
+
+	case 4:
+		application->setScene(Factory::GetInstance()->createTextureScene());
+		break;
+
+	case 5:
+		application->setScene(Factory::GetInstance()->createSkyBoxScene());
+		break;
+	}
+	application->Run();
+
+}
